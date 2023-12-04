27646d2f9e8e3c45232c2fedd2ea019f4d4ded49
Konstantin Läufer <laufer@cs.luc.edu>
2023-12-04 13:37:44 -0600

    simplified day 4 a bit more
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -2,7 +2,7 @@ import scala.collection.immutable.Queue
 
 object Day4:
 
-  def example =
+  val example =
     import scala.language.unsafeNulls
     """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
       |Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -10,7 +10,7 @@ object Day4:
       |Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
       |Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
       |Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
-    .stripMargin.split("\n").iterator.toIndexedSeq
+    .stripMargin.split("\n").iterator.map(lineToCard).toIndexedSeq
 
   type Card = (Set[Int], Set[Int])
 
@@ -20,11 +20,8 @@ object Day4:
     val card(n, w, a) = line: @unchecked
     (number.findAllIn(w).map(_.toInt).toSet, number.findAllIn(a).map(_.toInt).toSet)
 
-  def addCardValues(input: IndexedSeq[String]): Int =
-    input
-      .map(lineToCard)
-      .map((w, a) => math.pow(2, w.intersect(a).size - 1).toInt)
-      .sum
+  def addCardValues(cards: IndexedSeq[Card]): Int =
+    cards.map((w, a) => math.pow(2, w.intersect(a).size - 1).toInt).sum
 
   def processCards(cards: IndexedSeq[Card]): Int =
     Iterator.iterate(Queue(cards.zipWithIndex*)): q0 =>
@@ -33,10 +30,10 @@ object Day4:
       .takeWhile(_.nonEmpty)
       .length
 
-  def input = scala.io.Source.fromFile("data/day4Input.txt").getLines().toIndexedSeq
+  val input = scala.io.Source.fromFile("data/day4Input.txt").getLines().map(lineToCard).toIndexedSeq
 
   def main(args: Array[String]): Unit =
     println(s"day 4 part 1 example: ${addCardValues(example)}")
     println(s"day 4 part 1 solution: ${addCardValues(input)}")
-    println(s"day 4 part 2 example: ${processCards(example.map(lineToCard))}")
-    println(s"day 4 part 2 solution: ${processCards(input.map(lineToCard))}")
+    println(s"day 4 part 2 example: ${processCards(example)}")
+    println(s"day 4 part 2 solution: ${processCards(input)}")
